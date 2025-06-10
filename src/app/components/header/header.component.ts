403cd3af5f924f3acd1e64329ba68fb796c0000d
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2025-06-10 22:12:28 +0400

    Added functionality to fetch and display current date and time

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  currDateTime = new Date();
+  intervalId;
 
+  constructor() {
+    this.intervalId = setInterval(() => {
+      this.currDateTime = new Date();
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
 }
