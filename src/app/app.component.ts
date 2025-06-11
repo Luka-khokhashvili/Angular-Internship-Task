eefec0a6ab24fccc902d54ce389ca3021b5e1b49
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2025-06-11 21:22:01 +0400

    Add final structure and styles to user table component, functionality to be added later

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { UsersTableComponent } from './components/users-table/users-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, NavigationComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    NavigationComponent,
+    UsersTableComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
