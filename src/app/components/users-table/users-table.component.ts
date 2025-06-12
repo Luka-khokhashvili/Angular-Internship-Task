c53535c55433019079cbb9752feb3aded1b7988f
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2025-06-12 19:38:12 +0400

    Add search functionality to users. Can search by name, lastname or email.

--- src/app/components/users-table/users-table.component.ts
+++ src/app/components/users-table/users-table.component.ts
@@ -3,21 +3,35 @@ import { User } from '../../interfaces/user';
 import { CommonModule } from '@angular/common';
 import { UsersService } from '../../services/users.service';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-users-table',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './users-table.component.html',
   styleUrl: './users-table.component.scss',
 })
 export class UsersTableComponent {
   users: User[] = [];
+  searchTerm: string = '';
+  filteredUsers: User[] = [];
 
   constructor(private UsersService: UsersService) {}
 
   ngOnInit() {
-    this.UsersService.getUsers().subscribe((data) => {
-      this.users = data;
+    this.UsersService.getUsers().subscribe((users) => {
+      this.users = users;
+      this.filteredUsers = users;
     });
+  }
+
+  filterUsers() {
+    const term = this.searchTerm.toLowerCase();
+    this.filteredUsers = this.users.filter(
+      (user) =>
+        user.firstName.toLowerCase().includes(term) ||
+        user.lastName.toLowerCase().includes(term) ||
+        user.email.toLowerCase().includes(term)
+    );
   }
 }
