0809b36150d3cfd270404381615d7b116531a960
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2025-06-13 15:09:28 +0400

    Add footer component, styles. Footer sticks to bottom or scrolls with content, depending on the content height.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
-import { UsersTableComponent } from './components/users-table/users-table.component';
-import { PostsTableComponent } from './components/posts-table/posts-table.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, NavigationComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
